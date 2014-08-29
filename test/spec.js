16081b935f4a14cc9db2b08b916a352941805532
Daniel Husar <dano.husar@gmail.com>
2014-08-29 11:33:12 +0100

    tests

--- test/spec.js
+++ test/spec.js
@@ -4,8 +4,19 @@ var should = chai.should();
 
 
 describe('Globals', function() {
-  it('Libraries tests.', function () {
+  it('Libraries exists.', function () {
     $.should.be.a('function');
+    $.fn.mobileMenu.should.be.a('function');
+  });
+
+  it('It should add class to the html', function () {
+    $('button').trigger('click.menu');
+    $('html').hasClass('menu-open').should.be.equal(true);
+  });
+
+  it('It should remove class from the html', function () {
+    $('button').trigger('click.menu');
+    $('html').hasClass('menu-open').should.be.equal(false);
   });
 });
 
